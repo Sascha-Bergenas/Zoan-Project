48575a1ce8eefed220f82d481388e78253a200dd
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-01-13 11:49:53 +0100

    feat(card): add img prop to Card component

---
+++ my-react-app/src/components/ui/Card.jsx
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+=======
+import "./Card.css";
+
+export default function Card({ title, img, description, children }) {
+  return (
+    <div className="card">
+      <h3 className="card-title">{title}</h3>
+      <img className="card-img" src={img} alt={title} />
+      <p className="card-description">{description}</p>
+      {children}
+    </div>
+  );
+}
+>>>>>>> 990e7b8 (feat(card): add img prop to Card component)
