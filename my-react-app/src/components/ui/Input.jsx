39536dbf8544c98ef0c3007cdd55da0c3859231f
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-01-13 20:02:20 +0100

    input styling

--- my-react-app/src/components/ui/Input.jsx
+++ my-react-app/src/components/ui/Input.jsx
@@ -1,9 +1,13 @@
+import "./Input.css";
+
 const Input = ({ label, type = "text", placeholder, value, onChange }) => {
+  const inputClass = "input";
   return (
     <label>
       {label}
       <input
         type={type}
+        className={inputClass}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
