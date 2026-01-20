5df6c9a33b71636337c1fd75fcc6f375222679c9
Max Mattiasson <max@mattiasson.eu>
2026-01-20 12:59:06 +0100

    bingelibong

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import Dashboard from "./pages/DashboardPage";
+import { Routes, Route } from "react-router-dom";
 
 //Använd export default för att exportera en komponent från en fil
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Dashboard />
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      {/* <Route path="/ststass" element={<statsssss />} /> */}
+    </Routes>
     </>
   );
 }
--- my-react-app/src/pages/DashboardPage.jsx
+++ my-react-app/src/pages/DashboardPage.jsx
@@ -1,10 +1,15 @@
 import BaseCard from "../components/ui/Card";
+import Header from "../components/layout/Header";
 
 function Dashboard() {
   return (
     <>
+      <Header></Header>
       <BaseCard>
         <p>Testar</p>
+        <h1>bing
+          
+        </h1>
       </BaseCard>
     </>
   );
