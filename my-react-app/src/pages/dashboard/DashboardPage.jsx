df9ab3506db5bfc307152663e4b003d3d47083ac
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-01-23 13:41:11 +0100

    grid-nado

--- my-react-app/src/pages/dashboard/DashboardPage.jsx
+++ my-react-app/src/pages/dashboard/DashboardPage.jsx
@@ -5,32 +5,35 @@ import WorkSessionForm from "../../components/ui/form/WorkSessionForm";
 function Dashboard() {
   return (
     <>
-      <div className={styles.container}>
-        <BaseCard className={styles.timer} size="card-timer">
-          <p>CARD TIMER</p>
-        </BaseCard>
+      {" "}
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <BaseCard className={styles.timer} size="card-timer">
+            <h3>CARD TIMER</h3>
+          </BaseCard>
 
-        <BaseCard className={styles.card1} size="card-large">
-          <p>CARD 1</p>
-        </BaseCard>
+          <BaseCard className={styles.card1} size="card-large">
+            <h3>CARD 1 Status</h3>
+          </BaseCard>
 
-        <BaseCard className={styles.card2} size="card-medium">
-          <p>CARD 2</p>
-        </BaseCard>
+          <BaseCard className={styles.card2} size="card-medium">
+            <h3>CARD 2 Smarta rekommendationer</h3>
+          </BaseCard>
 
-        <BaseCard className={styles.card3} size="card-small">
-          <p>CARD 3</p>
-        </BaseCard>
+          <BaseCard className={styles.card3} size="card-small">
+            <h3>CARD 3 Mood</h3>
+          </BaseCard>
 
-        <BaseCard className={styles.card4} size="card-small">
-          <h3>Logga Arbetspass</h3>
-          <WorkSessionForm />
-        </BaseCard>
+          <BaseCard className={styles.card4} size="card-small">
+            <h3>Logga Arbetspass</h3>
+            <WorkSessionForm />
+          </BaseCard>
 
-        <BaseCard className={styles.card5} size="card-small">
-          <h3>Kalender</h3>
-        </BaseCard>
-      </div>
+          <BaseCard className={styles.card5} size="card-small">
+            <h3>Kalender</h3>
+          </BaseCard>
+        </div>
+      </main>
     </>
   );
 }
