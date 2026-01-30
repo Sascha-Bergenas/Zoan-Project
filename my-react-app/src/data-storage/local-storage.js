915a04481d0d18e806b8f05b8ae987ece56c1d1b
bjorneboman <gostabohman@gmail.com>
2026-01-30 21:38:57 +0100

    Read/Write from/to localStorage with a useLocalStorage hook

--- my-react-app/src/data-storage/local-storage.js
+++ my-react-app/src/data-storage/local-storage.js
@@ -1,7 +1,21 @@
-// Save function
+export function useLocalStorage(data = null) {
+    let storedData = null
 
-export function saveToLocalStorage(data = {}) {
-    console.log("I received this stuff:", data)
+    typeof data === "object" && data ? saveToLS(data) : readFromLS()
+    
+    // Save function
+    function saveToLS(newData) {
+        data = JSON.parse(localStorage.getItem("zoanSessions"))
+        if(!data) data = []
+        data.push(newData)
+        localStorage.setItem("zoanSessions", JSON.stringify(data))
+    }
+    
+    // Read function
+    function readFromLS() {
+        storedData = JSON.parse(localStorage.getItem("zoanSessions"))
+    }
+
+    return storedData
 }
 
-// Read function
