0cacd0cee25ddc5b0a8583f6d91920ba62d5c1d4
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-02-04 19:09:24 +0100

    feat(theme): add theme context and dark mode toggle

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,12 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import History from "./pages/HistoryPage";
 import Dashboard from "./pages/dashboard/DashboardPage";
 import Header from "./components/layout/Header";
-import Timer from "./components/Features/timer/Timer";
-import Topbar from "./components/layout/Topbar";
-
-//Använd export default för att exportera en komponent från en fil
+import { useContext } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
+import { useEffect } from "react";
 
 export default function App() {
+  const { theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
     <>
       <Header />
--- my-react-app/src/components/layout/Header.jsx
+++ my-react-app/src/components/layout/Header.jsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
-import LoginModal from "../../components/Features/modals/loginModal/loginModal";
+import LoginModal from "../Features/modals/loginModal/LoginModal";
 import { useAuth } from "../../contexts/useAuth";
 import Button from "../ui/Button";
 import "./header.css";
+import { useContext } from "react";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 export default function Header() {
   const { isAuthed, user, signOut } = useAuth();
+  const { toggleTheme } = useContext(ThemeContext);
 
   return (
     <header>
       <nav>
         <h1 className="text-lg">Zoan</h1>
         <ul className="text-md">
+          <li>
+            <button onClick={toggleTheme}>Dark Mode</button>
+          </li>
           <li>
             <Link to="/">Dashboard</Link>
           </li>
