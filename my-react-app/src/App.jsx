ed0aa4674326e85b2fbeb8a20a3369eaaecd81ac
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-02-04 13:33:38 +0100

    feat: remove statusPanel

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import History from "./pages/HistoryPage";
 import Dashboard from "./pages/dashboard/DashboardPage";
 import Header from "./components/layout/Header";
-import StatusPanel from "./components/Features/status/StatusPanel";
 import Timer from "./components/Features/timer/Timer";
 import Topbar from "./components/layout/Topbar";
 
@@ -18,7 +17,6 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/history" element={<History />} />
       </Routes>
-      <StatusPanel />
     </>
   );
 }
