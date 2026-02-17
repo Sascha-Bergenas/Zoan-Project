968436a102dfc605a439826fe59a412058fcac74
Max Mattiasson <max@mattiasson.eu>
2026-02-17 09:56:04 +0100

    mode in topbar done

--- my-react-app/src/components/layout/Topbar.jsx
+++ my-react-app/src/components/layout/Topbar.jsx
@@ -2,15 +2,39 @@ import TopBarCard from "./TopBarCard";
 import styles from "./Topbar.module.css";
 import { calcTime } from "../../utils/formatTime";
 import { useTimer } from "../../contexts/TimerContext";
+import { useState, useEffect } from "react";
 
 
 export default function Topbar() {
-  const { state, now } = useTimer();
+  const { state } = useTimer();
   const startedAt = state.firstStartedAtMs;
+  const [now, setNow] = useState(() => Date.now());
+
+  // Calculate and display total time
+  useEffect(() => {
+    if (state.firstStartedAtMs == null) return;
+  
+    const id = setInterval(() => setNow(Date.now()), 1000);
+    return () => clearInterval(id);
+  }, [state.firstStartedAtMs]);
 
   const totalTimeMs =
     startedAt != null ? Math.max(0, now - startedAt) : 0;
   const { formattedHours, formattedMinutes } = calcTime(totalTimeMs);
+
+  // Display mode
+  function formatMode(mode) {
+    if (!mode) return "-";
+  
+    switch (mode) {
+      case "deep":
+        return "Deep Work";
+      case "meeting":
+        return "Meeting";
+      case "chill":
+        return "Chill";
+    }
+  }
 
   return (
     <aside className={styles.container}>
@@ -26,7 +50,7 @@ export default function Topbar() {
         <p>om 45min</p>
       </TopBarCard>
       <TopBarCard title="Nuvarande mode" className={styles.card4}>
-        <p>Deep work</p>
+        <p>{formatMode(state.mode)}</p>
       </TopBarCard>
     </aside>
   );
